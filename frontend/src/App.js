2d195775cc9bdc04aab42c466dbc64cd20078279
Enxhi Tabaku <enxhi.tabaku@gmail.com>
2023-07-17 01:13:04 +0200

    Implemented the Navigation Bar and the res[ective route links.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,48 @@
 import './App.css'
+import NavigationBar from './shared/components/NavigationBar'
 import Users from './user/pages/Users'
+import AvatarOne from './static/images/avatar/avatar-boy-1.png'
+import AvatarTwo from './static/images/avatar/avatar-boy-2.png'
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+} from 'react-router-dom'
+
+const DummyUsersList = [
+    {
+        id: '1',
+        fullName: 'Remy John',
+        imageSrc: AvatarOne,
+        placeCount: 2,
+    },
+    {
+        id: '2',
+        fullName: 'John Sina',
+        imageSrc: AvatarTwo,
+        placeCount: 1,
+    },
+]
 
 function App() {
-    return <Users />
+    return (
+        <section id="trave-albania-main-container">
+            <Router>
+                <NavigationBar user={DummyUsersList[0]} />
+                <Switch>
+                    <Route path="/" exact>
+                        <Users />
+                    </Route>
+                    <Route path="/my-places" exact>
+                        <h1>My Places</h1>
+                    </Route>
+                    <Redirect to="/" />
+                </Switch>
+            </Router>
+        </section>
+    )
 }
 
 export default App
