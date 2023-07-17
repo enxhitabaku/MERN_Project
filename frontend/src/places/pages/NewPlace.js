0fabc4c0521096b9ad0453d1e2a01dd08427be0b
Enxhi Tabaku <enxhi.tabaku@gmail.com>
2023-07-18 01:38:04 +0200

    Core UI Implementation of the form component.

---
+++ frontend/src/places/pages/NewPlace.js
@@ -0,0 +1,64 @@
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
+import { Button, CardActions } from '@mui/material'
+
+export default function AddPlace() {
+    return (
+        <section id="new-place-section">
+            <Card>
+                <CardContent>
+                    <Box
+                        component="form"
+                        noValidate
+                        autoComplete="off"
+                        id="form-container"
+                    >
+                        <TextField
+                            required
+                            id="title-field"
+                            label="Title"
+                            defaultValue=""
+                        />
+                        <TextField
+                            id="description-field"
+                            label="Description"
+                            multiline
+                            rows={8}
+                            defaultValue=""
+                            variant="filled"
+                        />
+                        <div id="coordinates-fields-container">
+                            <TextField
+                                required
+                                id="latitude-field"
+                                label="Latitude"
+                                type="number"
+                            ></TextField>
+                            <TextField
+                                required
+                                id="longtitude-field"
+                                label="Longtitude"
+                                type="number"
+                            ></TextField>
+                        </div>
+                    </Box>
+                </CardContent>
+                <CardActions>
+                    <div
+                        id="form-action-buttons-container"
+                        className="important-action-buttons-container"
+                    >
+                        <Button size="small" variant="contained">
+                            Save
+                        </Button>
+                        <Button size="small" variant="contained" color="error">
+                            Discard
+                        </Button>
+                    </div>
+                </CardActions>
+            </Card>
+        </section>
+    )
+}
